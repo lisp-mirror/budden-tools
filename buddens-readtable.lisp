26b87ca1596313ec13a9846205ec14ac56f4e4a8
budyakDV <unknown>
2010-11-19 16:18:51 +0300

    Добавил defun, чтобы можно было встать на определение :buddens-readtable

--- buddens-readtable.lisp
+++ buddens-readtable.lisp
@@ -1,10 +1,16 @@
 (in-package :buddens-readtable)
-(defreadtable :buddens-readtable
+
+(cl-user::portably-without-package-locks
+(defun :buddens-readtable () "это не функция, а readtable" (error "this is not a function"))
+)
+
+(defreadtable :buddens-readtable 
   (:merge #+ccl :current)
   (:dispatch-macro-char #\# #\L #'sharpL-reader)
   (:dispatch-macro-char #\# #\. #'sbcl-sharp-dot)
   ; (:dispatch-macro-char #\# #\" #'sharp-double-quote-readmacro)
   )
+
 
 (defreadtable :buddens-readtable-a
   (:merge #+ccl :current)
