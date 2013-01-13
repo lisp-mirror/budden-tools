3b83539753e969fd0317ae24f2df39ea6ad71f94
budyakDV <unknown>
2013-01-14 00:54:12 +0400

    buddens-readtable=buddens-readtable-a

--- buddens-readtable.lisp
+++ buddens-readtable.lisp
@@ -4,7 +4,7 @@
 (defun :buddens-readtable-a () "Dummy function to be able to navigate to readtable definition with M-." (error "this is not a function"))
 )
 
-(defreadtable :buddens-readtable 
+(defreadtable :buddens-readtable  ; то же, что и buddens-readtable-a
   (:merge #+ccl :current)
   (:dispatch-macro-char #\# #\L #'sharpl-reader)
   (:dispatch-macro-char #\# #\. #'sbcl-sharp-dot)
