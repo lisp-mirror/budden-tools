c4053490ad73d58bef99bdaddd4f4a93583aecf5
budyakDV <unknown>
2013-02-02 01:26:32 +0400

    seem to load again under lispworks6.1

--- budden-load.lisp
+++ budden-load.lisp
@@ -1,5 +1,7 @@
 ;;; -*- Encoding: utf-8; -*-
 
+(in-package :cl-user)
+
 #+(and (not quicklisp) win32) (load "c:/lisp/quicklisp/setup.lisp")
 (ql:quickload '("cl-fad" "cl-ppcre" "md5" "alexandria" "cl-utilities" "named-readtables" "swank" "split-sequence"))
 
@@ -15,11 +17,10 @@
   (SETQ *READTABLE* (COPY-READTABLE *READTABLE*))
   (SETF (READTABLE-CASE *READTABLE*) :UPCASE))
 
-(pushnew :budden *features*)
 (pushnew :russian *features*)
 (pushnew :forbidden-symbol-names *features*) 
 
-(defvar *lisp-root* #+win32 "c:/lisp/def-symbol-readmacro" 
+(defvar *lisp-root* #+win32 "c:/lisp/def-symbol-readmacro/" 
         #-win32 
         (namestring 
          (merge-pathnames 
@@ -31,54 +32,27 @@
   (declare (type string path))
   (concatenate 'string *lisp-root* path))
 
-; (load (at-lisp-root "config.lisp"))
-
-(defmacro portably-without-package-locks (&body body)
-`(#+sbcl sb-ext:without-package-locks
-#+allegro excl::without-package-locks
-#+cmu ext:without-package-locks
-#+lispworks let 
-#+lispworks 
-((lw:*handle-warn-on-redefinition* :warn)
- (*packages-for-warn-on-redefinition* nil))
-#+clisp ext:without-package-lock #+clisp ()
-#-(or allegro lispworks sbcl clisp cmu) 
-progn
-,@body))
-
-;(defun asdf::! (&rest args) (apply 'asdf:load-system args))
-;(push "c:/lisp/def-symbol-readmacro/" asdf:*central-registry*)
+(pushnew *lisp-root* asdf:*central-registry*)
 
 (load (at-lisp-root "asdf2-tools.lisp"))
-(export 'asdf::load-system-by-asd-file :asdf)
+(export 'asdf::load-system-by-asd-file :asdf) ; это и правда нужно? 
 
 (asdf::! :decorate-function)
 (asdf::! :iterate-keywords)
-
-(defpackage :iterk
-  (:nicknames :iterate-keywords-tiny-export)
-  (:use #:cl #:iterate-keywords)
-  (:import-from #:iterate-keywords #:keywordize)
-  (:export #:iter #:dsetq #:keywordize))
-
-(asdf::! :see-packages)
 (asdf::! :budden-tools)
+(asdf::! :see-packages)
 (asdf::! :russian-budden-tools)
 
-;(load "load-js.lisp")
-;(break)
-;(asdf::! :anaphora)
 
+; устанавливаем, что symbol-readmacros могут начинаться с кириллических букв
 (setf budden-tools::*def-symbol-reamacro-additional-name-starting-characters*
       (append budden-tools::*def-symbol-reamacro-additional-name-starting-characters*
               russian-budden-tools::*cyrillic-characters*))
 
+; добавляем расширения к экспериментальной таблице чтения
 (budden-tools::ENABLE-BUDDENS-READTABLE-EXTENSIONS :buddens-readtable)
-(budden-tools::ENABLE-BUDDENS-READTABLE-EXTENSIONS :buddens-readtable-a)
-
-;(setf (budden-tools::readtable-case-advanced :buddens-readtable) :upcase-if-uniform)
-;(budden-tools:see-packages-on :buddens-readtable-a)
-(setf (budden-tools::readtable-case-advanced :buddens-readtable-a) :upcase-if-uniform)
-(budden-tools::in-readtable :buddens-readtable-a)
+; включаем чтение CamelCase 
+(setf (budden-tools::readtable-case-advanced :buddens-readtable) :upcase-if-uniform)
+(budden-tools::in-readtable :buddens-readtable)
 (setf *print-case* :downcase)
 #+lispworks6 (setf SYSTEM:*PRINT-SYMBOLS-USING-BARS* t)
