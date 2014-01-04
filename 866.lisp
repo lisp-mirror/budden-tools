e1513e864b303b8e8a0f3388966844feeee9416f
budyakDV <unknown>
2014-01-04 18:42:10 +0400

    Example of 866 codepage file attribute line

---
+++ 866.lisp
@@ -0,0 +1,6 @@
+;;; -*- Encoding: (win32:code-page :id 866 :eof-style :crlf);  -*-
+; Пример файла в кодировке 866. 
+; См. также editor::PROCESS-FILE-OPTIONS , system::file-encoding-option, s
+; Lispworks user guide and reference manual/22.6.3 guessing external formats and file streams
+; and look for "attribute line" in Lispworks docs. 
+
