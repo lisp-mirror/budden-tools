be4e04283b837d2c7a438cf24c1f221e549922a0
budyakDV <unknown>
2016-01-07 16:59:52 +0300

    Включил в :budden-tools sbcl-target-package-l2. Тест при загрузке проходит (тест запускается из clcon)

--- budden-tools.asd
+++ budden-tools.asd
@@ -18,8 +18,9 @@
    (:file "def-merge-packages"
     :description "defpackage with some new clauses which are useful in conjunction with this library. See def-merge-packages::!"
     )
+   (:file "sbcl-target-package-l2")
    (:file "defpackage-l2"
-    :description "Упрощённая версия def-merge-package для l2/s2")
+    :description "Упрощённая версия def-merge-package для l2/s2. Тест defpackage-l2-test запускается намного ниже из c:/clcon/lp/clcon/load-clcon-server-and-client-win32.lisp")
    (:file "package") ; might need
    (:file "package-proga-implementation")
    (:file "def-toplevel-progn")
--- defpackage-l2-test.lisp
+++ defpackage-l2-test.lisp
@@ -17,50 +17,31 @@
      (def-merge-packages:delete-package-metadata name)
      (delete-package name)))
 
-  (defpackage-l2::! :p1 (:always t) (:use :cl) (:export :sym :s1 :nth) (:print-defpackage-form t))
-  (defpackage-l2::! :p2 (:always t) (:use :cl) (:export :sym :s2 :nth))
-  (defpackage-l2::! :p1+p2 (:always t) (:print-defpackage-form t) (:use :p1 :p2) (:export :s1 :s2 nth))
+  (eval '(defpackage-l2::! :p1 (:always t) (:use :cl) (:export :sym :s1 :nth) (:print-defpackage-form t)))
+  (eval '(defpackage-l2::! :p2 (:always t) (:use :cl) (:export :sym :s2 :nth)))
+  (eval '(defpackage-l2::! :p1+p2 (:always t) (:print-defpackage-form t) (:use :p1 :p2) (:export :s1 :s2 nth)))
   )
 
 
-(defun ?? (name package list)
-  "errs if set of external symbols from package is not the same as list"
+(defun ?? (name package list-in-string)
+  "errs if set of external symbols from package is not the same as list. list must be represented as a string to read"
   (format t "~%Running ~A~%" name)
-  (let ((externals 
-         (iterk:iter
+  (let* ((externals 
+          (iterk:iter
            (:for s :in-package package :external-only t)
-           (:collect s))))
+           (:collect s)))
+         (list (read-from-string list-in-string)))
     (assert (null (set-exclusive-or externals list)))))
 
 (make-three-packages)
 
 ;;; Should warn here as p1:sym and p2:sym clash
-(?? 't1 :p1+p2 '(p1:s1 p2:s2 cl:nth))
+(?? 't1 :p1+p2 "(p1:s1 p2:s2 cl:nth)")
 
-;;; There is a way to suppress a warning: 
-(defpackage-l2::! :p1+p2 (:always t) (:use :p1 :p2) (:forbid :sym) (:export :s1 :s2 nth))
-(?? 't2 :p1+p2 '(p1:s1 p2:s2 cl:nth))
-
-;;;
-
-;;; Now an example of greater flexibility
-;;; We want package to use cl package. Also we want p1:sym there 
-;;; Also we want to export p2:sym. So, 
-
-; define intermediate package.
-(defpackage-l2::! :intermediate
-                       (:always t)
-                       (:use :p1 :p2)
-                       (:forbid :sym)
-                       (:export :s1 :s2))
-
-(?? 't4 :intermediate '(p1:s1 p2:s2))
-
-(let ((md (defpackage-l2:ensure-package-metadata :intermediate)))
+(let ((md (defpackage-l2:ensure-package-metadata :p1+p2)))
   (assert (eq (defpackage-l2:package-metadata-l2-package-p md) t))
   (assert (equalp (defpackage-l2:package-metadata-body-of-last-definition md)
-                  '((:ALWAYS T) (:USE :P1 :P2) (:FORBID :SYM)
-                    (:EXPORT :S1 :S2)))))
+                  '((:ALWAYS T) (:PRINT-DEFPACKAGE-FORM T) (:USE :P1 :P2) (:EXPORT :S1 :S2 NTH)))))
                   
 
 (defun got-buddens-readtable-a ()
@@ -94,6 +75,7 @@
            (let ((*readtable* (budden-tools:find-readtable :buddens-readtable-a))
                  (*package* (find-package :p1+p2)))
              (read-from-string "SYM"))))
+      (budden-tools::ignored result)
       (typep error 'error))
     t))
 
