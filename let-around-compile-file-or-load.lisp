9585e1beb30124e21d7069f97fa8d0cde613f802
budyakDV <unknown>
2016-05-01 15:58:20 +0400

    Переименование - связать-специальные-переменные-вокруг-load-И-compile-file

--- let-around-compile-file-or-load.lisp
+++ let-around-compile-file-or-load.lisp
@@ -4,21 +4,20 @@
 
 ;;; Любая переменная, упомянутая в соответствующей глобальной, будет пересвязана к самой себе или к значению БЫЛА-НЕ-СВЯЗАНА
 
-(def-merge-packages::! :СВЯЗАТЬ-СПЕЦИАЛЬНЫЕ-ПЕРЕМЕННЫЕ-ВОКРУГ-LOAD-ИЛИ-COMPILE-FILE
+(def-merge-packages::! :СВЯЗАТЬ-СПЕЦИАЛЬНЫЕ-ПЕРЕМЕННЫЕ-ВОКРУГ-LOAD-И-COMPILE-FILE
                        (:always t)
                        (:use :cl :budden-tools)
-                       (:shadow #:БЫЛА-НЕ-СВЯЗАНА #:*СВЯЗАТЬ-ВОКРУГ-COMPILE-FILE* #:*СВЯЗАТЬ-ВОКРУГ-LOAD*)
+                       (:shadow #:БЫЛА-НЕ-СВЯЗАНА #:*СПИСОК-ПЕРЕМЕННЫХ-ДЛЯ-СВЯЗЫВАНИЯ-ВОКРУГ-LOAD-И-COMPILE-FILE*)
                        (:export
                         "
-   СВЯЗАТЬ-СПЕЦИАЛЬНЫЕ-ПЕРЕМЕННЫЕ-ВОКРУГ-LOAD-ИЛИ-COMPILE-FILE:*СВЯЗАТЬ-ВОКРУГ-COMPILE-FILE*
-   СВЯЗАТЬ-СПЕЦИАЛЬНЫЕ-ПЕРЕМЕННЫЕ-ВОКРУГ-LOAD-ИЛИ-COMPILE-FILE:*СВЯЗАТЬ-ВОКРУГ-LOAD*
-   СВЯЗАТЬ-СПЕЦИАЛЬНЫЕ-ПЕРЕМЕННЫЕ-ВОКРУГ-LOAD-ИЛИ-COMPILE-FILE:БЫЛА-НЕ-СВЯЗАНА
+   СВЯЗАТЬ-СПЕЦИАЛЬНЫЕ-ПЕРЕМЕННЫЕ-ВОКРУГ-LOAD-И-COMPILE-FILE:*СПИСОК-ПЕРЕМЕННЫХ-ДЛЯ-СВЯЗЫВАНИЯ-ВОКРУГ-LOAD-И-COMPILE-FILE*
+   СВЯЗАТЬ-СПЕЦИАЛЬНЫЕ-ПЕРЕМЕННЫЕ-ВОКРУГ-LOAD-И-COMPILE-FILE:БЫЛА-НЕ-СВЯЗАНА
                          "
                         ))
 
-(in-package :СВЯЗАТЬ-СПЕЦИАЛЬНЫЕ-ПЕРЕМЕННЫЕ-ВОКРУГ-LOAD-ИЛИ-COMPILE-FILE)
+(in-package :СВЯЗАТЬ-СПЕЦИАЛЬНЫЕ-ПЕРЕМЕННЫЕ-ВОКРУГ-LOAD-И-COMPILE-FILE)
 
-(defparameter *СВЯЗАТЬ-ВОКРУГ-COMPILE-FILE* nil "Список переменных, к-рые будут связаны своими значениями вокруг compile-file")
+(defparameter *СПИСОК-ПЕРЕМЕННЫХ-ДЛЯ-СВЯЗЫВАНИЯ-ВОКРУГ-LOAD-И-COMPILE-FILE* nil "Список переменных, к-рые будут связаны своими значениями вокруг compile-file")
 
 (defun создать-фун-пересвязывающую-и-вызывающую-фун1 (фун1 список-имён)
   "фун1 должно быть объектом-функцией"
@@ -33,11 +32,12 @@
               (apply ,фун1 ,args))))))))
 
 (defun decorated-compile-file-let-around-compile-file-or-load (fn &rest args)
-  (let ((фун (создать-фун-пересвязывающую-и-вызывающую-фун1 fn *СВЯЗАТЬ-ВОКРУГ-COMPILE-FILE*)))
+  (let ((фун (создать-фун-пересвязывающую-и-вызывающую-фун1 fn *СПИСОК-ПЕРЕМЕННЫХ-ДЛЯ-СВЯЗЫВАНИЯ-ВОКРУГ-LOAD-И-COMPILE-FILE*)))
     (apply фун args)))
 
 (sb-ext::without-package-locks
  (decorate-function:decorate-function 'compile-file #'decorated-compile-file-let-around-compile-file-or-load)
+ (decorate-function:decorate-function 'load #'decorated-compile-file-let-around-compile-file-or-load)
  )
 
 
