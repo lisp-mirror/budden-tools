3aa2d1d7cf8f9c54483be6fe4c2718e99bc529a3
budyakDV <unknown>
2016-08-04 21:20:44 +0300

    Убрал некорректный комментарий

--- lexem-pos.lisp
+++ lexem-pos.lisp
@@ -19,7 +19,7 @@
 (in-package :lexem-pos)
 (named-readtables::in-readtable nil)
 
-(defstruct lexem-pos file-name start end start-line-number) ; FIXME - записпользовать
+(defstruct lexem-pos file-name start end start-line-number) 
 
 (defmethod cl:make-load-form ((self lexem-pos) &optional env)
   (make-load-form-saving-slots self :environment env))
