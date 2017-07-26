348f1dfdbd8f6afcfebc58aff80a82314095a1d6
budyakDV <unknown>
2017-07-27 02:16:58 +0300

    док

--- lispworks-ide-buddens-tools/��-��-��.lisp
+++ lispworks-ide-buddens-tools/��-��-��.lisp
@@ -7,7 +7,7 @@
   (:documentation "Открыть в редакторе файл и перейти на строку (считая с 1) и колонку (считая с 1) - нужно иметь в виду, что в text widget в tcl колонки считаются с 0, а end - это конец строки"))
 
 (defgeneric goto-offset (PATHNAME OFFSET &KEY KILL-BUFFER set-foreground-window subtract-no-of-newlines)
-  (:documentation "Открыть в редакторе файл и перейти на строку и колонку"))
+  (:documentation "Открыть в редакторе файл и перейти на позицию, считая с 1"))
 
 
 (defgeneric real-point-offset (point)
